3c47e3a12f4a8b1b30f5116ebf48544fa906b19f
NasimbayevFarhod <105714502+NasimbayevFarhod@users.noreply.github.com>
2022-08-12 22:27:18 +0500

    footer section finished

--- js/main.js
+++ js/main.js
@@ -1,7 +1,18 @@
 var elModalOpenButton = document.querySelector(".nav");
 var elModalOpenlink = document.querySelector(".navbar");
-
+var elBody = document.querySelector("body")
 
 elModalOpenButton.addEventListener("click", function () {
-  elModalOpenlink.classList.remove("nav--active");
+  elModalOpenlink.classList.toggle("nav--active");
 });
+
+elModalOpenButton.addEventListener("click", function () {
+  elModalOpenButton.classList.toggle("close");
+});
+
+elModalOpenButton.addEventListener("click", function () {
+  elBody.classList.toggle("overflow");
+});
+
+
+
